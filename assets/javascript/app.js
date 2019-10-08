37c3960c391bf44b26db8ee34ec388947946f3c2
Shehryar Ehtesham <se318@rutgers.edu>
2019-10-07 21:37:01 -0400

    added to open weather

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-
+//global variables for openweather api
 var apiKey = "d593e2d9c9a4edb4bda4173346b7b4e7";
 var city = "";
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",Burundi&units=imperial&appid=" + apiKey;
@@ -12,10 +12,12 @@ $.ajax({
   .then(function(response) {
     console.log(response)
 
+    $("#todaysWeather").html(response.main.temp + " Degrees")
+    console.log(response.main.temp)
 
 
 
-  })
+  });
 
   $.ajax({
     url: queryUrl2,
@@ -24,11 +26,12 @@ $.ajax({
 
   .then(function(response) {
     console.log(response)
+    for(var i = 0; i <= list.length)
+    $("#day1").html(list[3].main.temp + " Degrees")
 
 
 
-
-  })
+  });
 
 
 

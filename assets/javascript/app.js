7ebc575d6146b42807017d687b32ede15fc8223e
Shehryar Ehtesham <se318@rutgers.edu>
2019-10-09 21:42:30 -0400

    added on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -102,50 +102,49 @@ function displayRestaurant() {
 
 displayRestaurant();
 
-$( document ).ready(function() {
-//global variables for openweather api
-var apiKey = "d593e2d9c9a4edb4bda4173346b7b4e7";
-var city = "New Brunswick";
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",Burundi&units=imperial&appid=" + apiKey;
-var queryUrl2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",Burundi&units=imperial&appid=" + apiKey;
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-  })
+$("#searchBtn").on("click", function () {
+    //global variables for openweather api
+    var apiKey = "d593e2d9c9a4edb4bda4173346b7b4e7";
+    var city = "New Brunswick";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",Burundi&units=imperial&appid=" + apiKey;
+    var queryUrl2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",Burundi&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
 
-  .then(function(response) {
-    console.log(response)
+        .then(function (response) {
+            console.log(response)
 
-    $("#todaysWeather").html(response.main.temp + " Degrees")
-    console.log(response.main.temp)
-
-
-
-  });
-
-  $.ajax({
-    url: queryUrl2,
-    method: "GET"
-  })
-
-  .then(function(response) {
-    console.log(response)
-    // for(var i = 0; i <= list.length; i++)
-    $("#day1").html(response.list[4].main.temp + " Degrees " + response.list[4].weather[0].main )
-    $("#day2").html(response.list[12].main.temp + " Degrees " + response.list[12].weather[0].main )
-    $("#day3").html(response.list[20].main.temp + " Degrees " + response.list[20].weather[0].main )
-    $("#day4").html(response.list[28].main.temp + " Degrees " + response.list[28].weather[0].main )
-    $("#day5").html(response.list[36].main.temp + " Degrees " + response.list[36].weather[0].main )
-    
-    console.log("5 day " + response.list[3].main.temp)
-    
+            $("#todaysWeather").html(response.main.temp + " Degrees")
+            console.log(response.main.temp)
 
 
-  });
+
+        });
+
+    $.ajax({
+        url: queryUrl2,
+        method: "GET"
+    })
+
+        .then(function (response) {
+            console.log(response)
+            $("#day1").html(response.list[4].main.temp + " Degrees " + response.list[4].weather[0].main)
+            $("#day2").html(response.list[12].main.temp + " Degrees " + response.list[12].weather[0].main)
+            $("#day3").html(response.list[20].main.temp + " Degrees " + response.list[20].weather[0].main)
+            $("#day4").html(response.list[28].main.temp + " Degrees " + response.list[28].weather[0].main)
+            $("#day5").html(response.list[36].main.temp + " Degrees " + response.list[36].weather[0].main)
 
 
 
 
 
-});
+        });
+
+
+
+
+
+})
 
